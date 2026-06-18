a6fb147bd6e75819a6c4a8df46169701a13d77ab
Mr.Sonu <114944221+sonukumar121@users.noreply.github.com>
2026-06-18 18:48:44 +0530

    Update user.js

--- Notesapp/Backend/model/user.js
+++ Notesapp/Backend/model/user.js
@@ -4,18 +4,18 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: false,
     },
 
     email: {
       type: String,
-      required: true,
+      required: false,
       unique:true
     },
 
      password: {
       type: String,
-      required: true,
+      required: false,
     },
 
   },
