5b0f07512dfa8c6ebcb2ef6845286cb399d09f22
Mr.Sonu <114944221+sonukumar121@users.noreply.github.com>
2026-06-18 17:35:56 +0530

    Update google.js

--- Notesapp/Backend/auth/google.js
+++ Notesapp/Backend/auth/google.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import dotenv from "dotenv";
-import User from "../model/user.js";
+import user from "../model/user.js";
 
 dotenv.config();
 
