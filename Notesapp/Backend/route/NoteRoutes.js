fd7c12cb82a83fdc6b36c2ce92bea47a8e1a5ff9
Mr.Sonu <114944221+sonukumar121@users.noreply.github.com>
2026-06-21 16:53:05 +0530

    Update NoteRoutes.js

--- Notesapp/Backend/route/NoteRoutes.js
+++ Notesapp/Backend/route/NoteRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 const Router = express.Router(); 
-import { addnote , getnote , deletenote , updatenote , searchnote} from "../controller/NoteController.js";
+import { addnote , getnote , deletenote , updatenote , searchnote,searchdate} from "../controller/NoteController.js";
 import {auth} from "../middlewares/userAuth.js"
 
 Router.post("/",auth,addnote);
 Router.get("/",auth,getnote);
 Router.put("/:id",auth,updatenote)
 Router.delete("/:id",auth,deletenote);
-Router.get("/search",auth,searchnote)
 
+Router.get("/search",auth,searchnote)
+Router.get("/date",auth,searchdate)
 
 
 export default Router;
