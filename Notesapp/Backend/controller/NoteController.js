195fdd078f7d64c2b9cafef099784bb36a86654b
Mr.Sonu <114944221+sonukumar121@users.noreply.github.com>
2026-06-21 16:27:53 +0530

    Update NoteController.js

--- Notesapp/Backend/controller/NoteController.js
+++ Notesapp/Backend/controller/NoteController.js
@@ -16,76 +16,76 @@ export const addnote=async(req,res)=>{
 
 
 export const searchnote = async (req, res) => {
-  try {
+//   try {
       
-    const { search } = req.query;
-      if(search=="") return;
-      console.log(search)
-    const query = {};
+//     const { search } = req.query;
+//       if(search=="") return;
+//       console.log(search)
+//     const query = {};
 
-    if (search) {
-      query.$or = [
-        {
-          title: {
-            $regex: search,
-            $options: "i",
-          },
-        },
-        {
-          description: {
-            $regex: search,
-            $options: "i",
-          },
-        },
-      ];
-           const notes = await Note.find(query);
-         console.log(notes);
-     return res.json({notes});
+//     if (search) {
+//       query.$or = [
+//         {
+//           title: {
+//             $regex: search,
+//             $options: "i",
+//           },
+//         },
+//         {
+//           description: {
+//             $regex: search,
+//             $options: "i",
+//           },
+//         },
+//       ];
+//            const notes = await Note.find(query);
+//          console.log(notes);
+//      return res.json({notes});
     
    
       
-    }
+//     }
 
-    res.status(400).json({ message: "search Data is required" });
-
-
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Server error" });
-  }
-}
+//     res.status(400).json({ message: "search Data is required" });
 
 
-
-
-
-//     try {
-//     const { search } = req.query;
-
-//     const notes = await expense.find({
-//       userid: req.user.id,
-//       $or: [
-//         {
-//           note: {
-//             $regex: search,
-//             $options: "i",
-//           },
-//         },
-//         {
-//           category: {
-//             $regex: search,
-//             $options: "i",
-//           },
-//         },
-//       ],
-//     });
-
-//     return res.json({ notes });
 //   } catch (err) {
 //     console.log(err);
 //     res.status(500).json({ message: "Server error" });
 //   }
 // }
+
+
+
+
+
+    try {
+    const { search } = req.query;
+
+    const notes = await expense.find({
+      userid: req.user.id,
+      $or: [
+        {
+          note: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+        {
+          category: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+      ],
+    });
+
+    return res.json({ notes });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
 
 
 
